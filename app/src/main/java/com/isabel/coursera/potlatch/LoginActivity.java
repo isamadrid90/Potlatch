5caa0b3b8c71ef1c164573ff1740a2aabc2fa576
Isabel <isa.madrid90@gmail.com>
2014-11-11 22:13:04 +0100

    First Commit

---
+++ app/src/main/java/com/isabel/coursera/potlatch/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.isabel.coursera.potlatch;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+
+}
