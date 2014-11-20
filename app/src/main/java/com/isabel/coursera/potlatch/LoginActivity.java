4b6b4089ec396e395b7e03e3692eb87af571598d
Isabel <isa.madrid90@gmail.com>
2014-11-20 21:33:01 +0100

    Added UI

--- app/src/main/java/com/isabel/coursera/potlatch/LoginActivity.java
+++ app/src/main/java/com/isabel/coursera/potlatch/LoginActivity.java
@@ -1,8 +1,10 @@
 package com.isabel.coursera.potlatch;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends Activity {
 
@@ -10,6 +12,24 @@ public class LoginActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button btn_login = (Button) findViewById(R.id.btn_login);
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO Funcion check login
+            }
+        });
+
+        Button btn_register = (Button) findViewById(R.id.btn_register);
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(),RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
 
